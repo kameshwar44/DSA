eb6c3d20a0e2f7aa759e50829d5caed847620ce8
Kameshwar Das <46256967+kameshwar44@users.noreply.github.com>
2023-08-12 13:48:17 +0530

    recent DSA

--- zTest/1.js
+++ zTest/1.js
@@ -1,12 +1,19 @@
-// const myPromise = new Promise((resolve,reject)=>{
+const myPromise = new Promise((resolve,reject)=>{
 
-//     let randomNum = Math.floor(Math.random()*10)
+    let randomNum = Math.floor(Math.random()*10)
 
-//     if(randomNum>5){
-//         console.log(`Success! Its Greater ${randomNum}`)
-//     }
-//     else{
-//         console.log(`Erorr Its Not ${randomNum}`);
-//     }
-// })
-// myPromise()
+    if(randomNum>5){
+        resolve(`Success! Its Greater ${randomNum}`)
+    }
+    else{
+        reject(`Error Its Not ${randomNum}`);
+    }
+},1000)
+
+myPromise.then((result)=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
